ece5599be278e509081039ae6895ece8f4761a00
Ivan Bautista <ivanbvigil@gmail.com>
2019-02-13 01:30:09 -0600

    aded the create page

--- prueba/urls.py
+++ prueba/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 import tasks.views
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', tasks.views.home, name = 'home'),
-    path('accounts/', include('accounts.urls'), name = 'accounts')
+    path('accounts/', include('accounts.urls'), name = 'accounts'),
+    path('tasks/', include('tasks.urls'), name = 'tasks'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Task
+from django.utils import timezone
 
 # Create your views here.
 
 def home(request):
 
 	return render(request, 'tasks/home.html')
+
+@login_required
+def create(request):
+	if request.method == 'POST':
+		if request.POST['title'] and request.POST['body'] and request.FILES['image']:
+			task = Task ()
+			task.title = request.POST['title']
+			task.body =  request.POST['body']
+
+			#if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+			#	task.url = request.POST['url']
+			#else:
+			#	task.url = 'http://' + request.POST['url']	
+			
+			task.image = request.FILES['image']
+			task.pub_date = timezone.datetime.now()
+			task.votes_total = 1
+			task.author = request.user
+			task.save()
+			return redirect('home')
+		else:
+			return render(request, 'tasks/create.html', {'error' : 'All files are required'})	
+	else:
+		return render(request, 'tasks/create.html')
