92cd038fcdceecdbb254a2c2fff7f40b215c6aaa
Ivan <ivan@Smarts-MacBook.local>
2019-02-14 01:12:36 -0600

    some polish

--- prueba/settings.py
+++ prueba/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap_modal_forms',
     'tasks.apps.TasksConfig',
     'accounts.apps.AccountsConfig',
     'django.contrib.admin',
